dd8179756235b580711c8388b8193395cda29136
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-08-16 18:11:13 +0300

    (chore) add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { CreateComponent } from './create/create.component';
+import { LayoutComponent } from './layout/layout.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { EditComponent } from './edit/edit.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    CreateComponent,
+    LayoutComponent,
+    RegistrationComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
