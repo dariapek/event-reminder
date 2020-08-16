2d6cfd77b9517ecd9ce6c28742f631563bed8989
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-08-16 18:10:54 +0300

    (chore) add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LayoutComponent} from './layout/layout.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {LoginComponent} from './login/login.component';
+import {RegistrationComponent} from './registration/registration.component';
+import {CreateComponent} from './create/create.component';
+import {EditComponent} from './edit/edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: LayoutComponent,
+  children: [
+    {
+      path: '',
+      redirectTo: '/',
+      pathMatch: 'full',
+    },
+    {
+      path: '',
+      component: DashboardComponent,
+    },
+    {
+      path: 'login',
+      component: LoginComponent,
+    },
+    {
+      path: 'registration',
+      component: RegistrationComponent,
+    },
+    {
+      path: 'create',
+      component: CreateComponent,
+    },
+    {
+      path: 'edit/:id',
+      component: EditComponent,
+    },
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
