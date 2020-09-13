d73c7d76d449af6bd728c3181afb79ff79f66ffe
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-09-13 09:45:51 +0300

    (chore) add close button and no events card

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {EventService} from '../event.service';
 import {EventItem} from '../interface';
@@ -11,14 +11,30 @@ import {EventItem} from '../interface';
 export class EventComponent implements OnInit {
   public eventSubscription: Subscription;
   public eventDate: EventItem;
+  public isEmptyMessage = false;
+  public isShow = false;
 
   constructor(private eventService: EventService) {
-    this.eventSubscription = eventService.event$.subscribe(event => {
-      this.eventDate = event;
-    });
+   this.openEvent();
   }
 
   ngOnInit(): void {
+  }
+
+  openEvent() {
+    this.eventSubscription = this.eventService.event$.subscribe(event => {
+      this.isShow = true;
+      if (!event) {
+        this.isEmptyMessage = true;
+      } else {
+        this.isEmptyMessage = false;
+        return this.eventDate = event;
+      }
+    });
+  }
+
+  close() {
+    this.isShow = false;
   }
 
 }
